a97d8e0a8ce4c682050e74c0c7403182a2753b80
Pieter Erzeel <pieter.erzeel@gmail.com>
2016-01-29 19:26:32 +0100

    added performace test again, got lost somewhere :/

---
+++ src/Test/Performance/PerformanceTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Physics.Test.Performance
+{
+    [TestClass]
+    public class PerformanceTest : GivenSiSystem
+    {
+        [TestMethod]
+        public void CheckPerformance()
+        {
+            var timeTaken = new Stopwatch();
+            timeTaken.Start();
+
+            for (var i = 0; i < 100000; i++)
+            {
+                var kWh = 1000*W*h;
+                var m3 = m ^ 3;
+
+                // 100 kWh
+                var energy = new Quantity(100, kWh);
+
+                // 5 m³
+                var volume = new Quantity(5, m3);
+
+                // 20 kWh/m³
+                var result = energy/volume;
+            }
+
+            Console.WriteLine(timeTaken.Elapsed);
+        }
+    }
+}
